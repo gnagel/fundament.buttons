f9eb21bd273aca3397fe6d69f7f88bd81ab485bf
felics <fspoettel@gmail.com>
2014-04-19 14:54:37 +0200

    0.0.1

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+
+// *    Gulpfile
+// * ---------------------
+// * This file contains the gulp build-process.
+// * ---------------------
+
+// *   Core
+// * ---------------------
+
+// * Require gulp modules
+
+var gulp          = require("gulp"),
+    cssmin        = require("gulp-minify-css"),
+    plumber       = require("gulp-plumber"),
+    rename        = require("gulp-rename"),
+    sass          = require("gulp-ruby-sass");
+
+// * scss
+// * 1. handle errors
+// * 2. sass
+// * 3. prefix
+// * --> write out
+// * 4. minify
+// * --> write out
+// * ---------------------
+
+gulp.task("sass", function () {
+  gulp.src("scss/fundament-buttons.scss")
+    .pipe(plumber({errorHandler: function(){console.log("Exiting")}}))
+    .pipe(sass({noCache: true}))
+    .pipe(rename("fundament-buttons.css"))
+    .pipe(gulp.dest("./dist"));
+});
+
+
+// * Tasks
+// * ---------------------
+
+// * Default task
+
+gulp.task("default", ["sass"]);
